c592e54809e6ab580e967633ce3b90f23b9ab7a6
amartdev <amartdev296@gmail.com>
2024-12-29 08:32:05 -0800

    stage-14-signup

--- lib/constants.ts
+++ lib/constants.ts
@@ -4,6 +4,9 @@ export const APP_SLOGAN =
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
   'ABUILT with Next.js, MongoDB,Tailwind CSS, and TypeScript'
+export const APP_COPYRIGHT =
+  process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
+  `Copyright © 2025 ${APP_NAME}. All rights reserved.`
 
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 
