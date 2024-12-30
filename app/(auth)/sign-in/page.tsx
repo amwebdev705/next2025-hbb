462a1d8ba19e14c1a9148800129da570b3c93b34
amartdev <amartdev296@gmail.com>
2024-12-29 17:35:31 -0800

    stage-16-google-signin

--- app/(auth)/sign-in/page.tsx
+++ app/(auth)/sign-in/page.tsx
@@ -2,13 +2,15 @@ import { Metadata } from 'next'
 import Link from 'next/link'
 import { redirect } from 'next/navigation'
 
-import {auth} from '@/auth'
+import { auth } from '@/auth'
 import SeparatorWithOr from '@/components/shared/separartor-or'
+
 import { Card, CardContent, CardHeader, CardTitle } from '@/components/ui/card'
 
 import CredentialsSignInForm from './credentials-signin-form'
 import { Button } from '@/components/ui/button'
 import { APP_NAME } from '@/lib/constants'
+import { GoogleSignInForm } from './google-signin-form'
 
 export const metadata: Metadata = {
   title: 'Sign In',
@@ -37,6 +39,10 @@ export default async function SignIn(props: {
         <CardContent>
           <div>
             <CredentialsSignInForm />
+          </div>
+          <SeparatorWithOr />
+          <div className="mt-4">
+            <GoogleSignInForm />
           </div>
         </CardContent>
       </Card>
