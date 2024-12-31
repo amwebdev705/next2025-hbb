664760d437cb61bcacd1388d13baf68f78959a39
amartdev <amartdev296@gmail.com>
2024-12-31 06:02:05 -0800

    stage-33-edit-user

--- lib/constants.ts
+++ lib/constants.ts
@@ -1,18 +1,14 @@
-export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'HBBOUTIQUENEXT'
+export const APP_NAME = process.env.NEXT_PUBLIC_APP_NAME || 'NxtAmzn'
 export const SERVER_URL =
   process.env.NEXT_PUBLIC_SERVER_URL || 'http://localhost:3000'
 
-
-// export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'HBB_Next25@resend.com'
-export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.com'
+export const SENDER_EMAIL = process.env.SENDER_EMAIL || 'onboarding@resend.dev'
 export const SENDER_NAME = process.env.SENDER_NAME || APP_NAME
-
-
 export const APP_SLOGAN =
-  process.env.NEXT_PUBLIC_APP_SLOGAN || 'All your eco-beauty needs'
+  process.env.NEXT_PUBLIC_APP_SLOGAN || 'Spend less, enjoy more.'
 export const APP_DESCRIPTION =
   process.env.NEXT_PUBLIC_APP_DESCRIPTION ||
-  'ABUILT with Next.js, MongoDB,Tailwind CSS, and TypeScript'
+  'An Amazon clone built with Next.js and MongoDB'
 export const APP_COPYRIGHT =
   process.env.NEXT_PUBLIC_APP_COPYRIGHT ||
   `Copyright © 2025 ${APP_NAME}. All rights reserved.`
@@ -20,19 +16,19 @@ export const APP_COPYRIGHT =
 export const PAGE_SIZE = Number(process.env.PAGE_SIZE || 9)
 
 export const FREE_SHIPPING_MIN_PRICE = Number(
-  process.env.FREE_SHIPPING_MIN_PRICE || 100
+  process.env.FREE_SHIPPING_MIN_PRICE || 35
 )
 
 export const AVAILABLE_PAYMENT_METHODS = [
   {
     name: 'PayPal',
     commission: 0,
-    isDefault: false,
+    isDefault: true,
   },
   {
     name: 'Stripe',
     commission: 0,
-    isDefault: true,
+    isDefault: false,
   },
   {
     name: 'Cash On Delivery',
@@ -41,7 +37,7 @@ export const AVAILABLE_PAYMENT_METHODS = [
   },
 ]
 export const DEFAULT_PAYMENT_METHOD =
-  process.env.DEFAULT_PAYMENT_METHOD || 'Stripe'
+  process.env.DEFAULT_PAYMENT_METHOD || 'PayPal'
 
 export const AVAILABLE_DELIVERY_DATES = [
   {
@@ -60,6 +56,8 @@ export const AVAILABLE_DELIVERY_DATES = [
     name: 'Next 5 Days',
     daysToDeliver: 5,
     shippingPrice: 4.9,
-    freeShippingMinPrice: 100,
+    freeShippingMinPrice: 35,
   },
 ]
+
+export const USER_ROLES = ['Admin', 'User']
